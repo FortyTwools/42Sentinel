9b03eab8b6e6b3e83079aa3c89b842de0114b2f5
linuxNeutrou <itsneutrouthetrou@gmail.com>
2025-12-28 16:41:10 +0100

    GET /api/v1/ft_users works!

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1.raw import router as raw_router
+from backend.app.api.v1.ft_users import router as raw_router
 from app.api.v1.health import router as health_router
 
 app = FastAPI(title="42Sentinel")
--- frontend/src/views/TestView.vue
+++ frontend/src/views/TestView.vue
@@ -6,7 +6,7 @@ import APITest from '../components/APITestCard.vue'
   <section>
     <APITest method="get" url="/api/v1/health" />
     <APITest method="post" url="/api/v1/raw/fetch/users" />
-    <APITest method="post" url="/api/v1/raw/fetch/users?campus=59" />
+    <APITest method="get" url="/api/v1/raw/fetch/users" />
     <APITest method="dskjfbqs" url="test" />
   </section>
 </template>
